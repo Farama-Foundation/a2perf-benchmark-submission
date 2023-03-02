88523dbdd4386243b5bea2cc4c14b2f7ca57f652
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2023-03-01 21:37:31 -0500

    Working training script

--- train.py
+++ train.py
@@ -2,54 +2,344 @@ import os
 import time
 
 import gin
-import numpy as np
+import tensorflow as tf  # pylint: disable=g-explicit-tensorflow-version-import
+import tf_agents.specs.array_spec
+from absl import logging
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.utils import common
+from tf_agents.specs import BoundedTensorSpec, TensorSpec
+from tf_agents.trajectories import PolicyStep
+from rl_perf.domains.web_nav.CoDE import q_networks
 
-from CoDE import test_websites
-from CoDE import utils
-from CoDE import vocabulary_node
-from CoDE import web_environment
-from CoDE import web_primitives
-from CoDE import q_networks
+
+class DQNLSTMtoTFA(q_networks.DQNWebLSTM):
+    """Converts the DQNWebLSTM to a TFA network."""
+
+    def __init__(self, *args, **kwargs):
+
+        self.losses = None
+        if kwargs.get('input_tensor_spec', None) is None:
+            raise ValueError('input_tensor_spec must be provided')
+        self.input_tensor_spec = kwargs['input_tensor_spec']
+
+        if kwargs.get('state_spec', None) is None:
+            self.state_spec = self.input_tensor_spec
+        else:
+            self.state_spec = kwargs['state_spec']
+            kwargs.pop('state_spec')
+        self._network_output_spec = None
+        kwargs.pop('input_tensor_spec')
+
+        # self._policy_state_spec = self.state_spec
+        self._policy_state_spec = ()
+        super().__init__(*args, **kwargs)
+        if kwargs.get('name', None) is None:
+            kwargs['name'] = 'DQNLSTMtoTFA'
+        self._name = kwargs['name']
+
+        self.trainable_weights = self.trainable_variables
+        self.non_trainable_weights = self.non_trainable_variables
+
+    def __call__(self, observations, training=False, network_state=(), step_type=()):
+        output = super().__call__(observation=observations, is_training=training)
+        if len(output) > 1:
+            q_values, value = output
+        else:
+            q_values = output[0]
+
+        return q_values, network_state
+
+    def _serialize_to_tensors(self):
+        pass
+
+    def create_variables(self, input_tensor_spec=None):
+        if self._network_output_spec is not None:
+            return self._network_output_spec
+        if self.input_tensor_spec is None:
+            self.input_tensor_spec = input_tensor_spec
+        input_tensor_spec = self.input_tensor_spec
+        if input_tensor_spec is None:
+            raise ValueError(
+                    "Unable to create_variables: no input_tensor_spec provided, and "
+                    "Network did not define one.")
+
+        random_input = tf.nest.map_structure(lambda spec: tf_agents.specs.sample_spec_nest(spec), input_tensor_spec)
+        random_input = tf.nest.map_structure(lambda x: tf.expand_dims(x, 0), random_input)
+
+        output, _ = self.__call__(random_input, training=False)
+
+        # Reset the trainable and non-trainable weights after __call__ creates them
+        self.trainable_weights = self.trainable_variables
+        self.non_trainable_weights = self.non_trainable_variables
+        return tf.TensorSpec.from_tensor(tf.squeeze(output))
+
+    def copy(self, name=None):
+        cp = DQNLSTMtoTFA(vocab_size=self._embedder.vocab_size,
+                          embedding_dim=self._embedding_dim,
+                          latent_dim=50,
+                          input_tensor_spec=self.input_tensor_spec,
+                          profile_value_dropout=1.0,
+                          q_min=None,
+                          q_max=None,
+                          return_state_value=True,
+                          name=name
+                          )
+        cp.create_variables()
+
+        for source_var, target_var in zip(self.variables, cp.variables):
+            target_var.assign(source_var)
+        return cp
+
+    def _restore_from_tensors(self, restored_tensors):
+        pass
+
+
+@gin.configurable
+def train_eval(
+        root_dir,
+        env_name='CartPole-v0',
+        num_iterations=100000,
+        train_sequence_length=1,
+
+        # Params for collect
+        initial_collect_steps=20,
+        collect_steps_per_iteration=1,
+        epsilon_greedy=0.1,
+        replay_buffer_capacity=100000,
+        # Params for target update
+        target_update_tau=0.05,
+        target_update_period=5,
+        # Params for train
+        train_steps_per_iteration=1,
+        batch_size=64,
+        learning_rate=1e-3,
+        n_step_update=1,
+        gamma=0.99,
+        reward_scale_factor=1.0,
+        gradient_clipping=None,
+        use_tf_functions=True,
+        # Params for eval
+        num_eval_episodes=10,
+        eval_interval=1000,
+        # Params for checkpoints
+        train_checkpoint_interval=10000,
+        policy_checkpoint_interval=5000,
+        rb_checkpoint_interval=20000,
+        # Params for summaries and logging
+        log_interval=1000,
+        summary_interval=1000,
+        summaries_flush_secs=10,
+        debug_summaries=False,
+        summarize_grads_and_vars=False,
+        eval_metrics_callback=None):
+    """A simple train and eval for DQN."""
+    root_dir = os.path.expanduser(root_dir)
+    train_dir = os.path.join(root_dir, 'train')
+    eval_dir = os.path.join(root_dir, 'eval')
+
+    train_summary_writer = tf.compat.v2.summary.create_file_writer(
+            train_dir, flush_millis=summaries_flush_secs * 1000)
+    train_summary_writer.set_as_default()
+
+    eval_summary_writer = tf.compat.v2.summary.create_file_writer(
+            eval_dir, flush_millis=summaries_flush_secs * 1000)
+    eval_metrics = [
+            tf_metrics.AverageReturnMetric(buffer_size=num_eval_episodes),
+            tf_metrics.AverageEpisodeLengthMetric(buffer_size=num_eval_episodes)
+            ]
+
+    global_step = tf.compat.v1.train.get_or_create_global_step()
+    with tf.compat.v2.summary.record_if(
+            lambda: tf.math.equal(global_step % summary_interval, 0)):
+        tf_env = tf_py_environment.TFPyEnvironment(suite_gym.load(env_name, gym_kwargs={'difficulty': 1}))
+        eval_tf_env = tf_py_environment.TFPyEnvironment(suite_gym.load(env_name, gym_kwargs={'difficulty': 1}))
+
+        if train_sequence_length != 1 and n_step_update != 1:
+            raise NotImplementedError(
+                    'train_eval does not currently support n-step updates with stateful '
+                    'networks (i.e., RNNs)')
+
+        action_spec = tf_env.action_spec()
+        num_actions = action_spec.maximum - action_spec.minimum + 1
+
+        q_net = DQNLSTMtoTFA(vocab_size=tf_env.pyenv.envs[0].env.local_vocab.max_vocabulary_size,
+                             profile_value_dropout=1.0,
+                             q_min=None,
+                             q_max=None,
+                             input_tensor_spec=tf_env.observation_spec(),
+                             state_spec=(),
+                             embedding_dim=100,
+                             latent_dim=50,
+                             return_state_value=True)
+
+        # TODO(b/127301657): Decay epsilon based on global step, cf. cl/188907839
+        tf_agent = dqn_agent.DqnAgent(
+                tf_env.time_step_spec(),
+                tf_env.action_spec(),
+                q_network=q_net,
+                epsilon_greedy=epsilon_greedy,
+                n_step_update=n_step_update,
+                target_update_tau=target_update_tau,
+                target_update_period=target_update_period,
+                optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate),
+                td_errors_loss_fn=common.element_wise_squared_loss,
+                gamma=gamma,
+                reward_scale_factor=reward_scale_factor,
+                gradient_clipping=gradient_clipping,
+                debug_summaries=debug_summaries,
+                summarize_grads_and_vars=summarize_grads_and_vars,
+                train_step_counter=global_step)
+        tf_agent.initialize()
+
+        train_metrics = [
+                tf_metrics.NumberOfEpisodes(),
+                tf_metrics.EnvironmentSteps(),
+                tf_metrics.AverageReturnMetric(),
+                tf_metrics.AverageEpisodeLengthMetric(),
+                ]
+
+        eval_policy = tf_agent.policy
+        collect_policy = tf_agent.collect_policy
+
+        replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+                data_spec=tf_agent.collect_data_spec,
+                batch_size=tf_env.batch_size,
+                max_length=replay_buffer_capacity)
+
+        collect_driver = dynamic_step_driver.DynamicStepDriver(
+                tf_env,
+                collect_policy,
+                observers=[replay_buffer.add_batch] + train_metrics,
+                num_steps=collect_steps_per_iteration)
+
+        train_checkpointer = common.Checkpointer(
+                ckpt_dir=train_dir,
+                agent=tf_agent,
+                global_step=global_step,
+                metrics=metric_utils.MetricsGroup(train_metrics, 'train_metrics'))
+        policy_checkpointer = common.Checkpointer(
+                ckpt_dir=os.path.join(train_dir, 'policy'),
+                policy=eval_policy,
+                global_step=global_step)
+        rb_checkpointer = common.Checkpointer(
+                ckpt_dir=os.path.join(train_dir, 'replay_buffer'),
+                max_to_keep=1,
+                replay_buffer=replay_buffer)
+
+        train_checkpointer.initialize_or_restore()
+        rb_checkpointer.initialize_or_restore()
+
+        if use_tf_functions:
+            # To speed up collect use common.function.
+            collect_driver.run = common.function(collect_driver.run)
+            tf_agent.train = common.function(tf_agent.train)
+
+        initial_collect_policy = random_tf_policy.RandomTFPolicy(
+                tf_env.time_step_spec(), tf_env.action_spec(), validate_args=True)
+
+        # Collect initial replay data.
+        logging.info(
+                'Initializing replay buffer by collecting experience for %d steps with '
+                'a random policy.', initial_collect_steps)
+        dynamic_step_driver.DynamicStepDriver(
+                tf_env,
+                initial_collect_policy,
+                observers=[replay_buffer.add_batch] + train_metrics,
+                num_steps=initial_collect_steps).run()
+
+        results = metric_utils.eager_compute(
+                eval_metrics,
+                eval_tf_env,
+                eval_policy,
+                num_episodes=num_eval_episodes,
+                train_step=global_step,
+                summary_writer=eval_summary_writer,
+                summary_prefix='Metrics',
+                )
+        if eval_metrics_callback is not None:
+            eval_metrics_callback(results, global_step.numpy())
+        metric_utils.log_metrics(eval_metrics)
+
+        time_step = None
+        policy_state = collect_policy.get_initial_state(tf_env.batch_size)
+
+        timed_at_step = global_step.numpy()
+        time_acc = 0
+
+        # Dataset generates trajectories with shape [Bx2x...]
+        dataset = replay_buffer.as_dataset(
+                num_parallel_calls=3,
+                sample_batch_size=batch_size,
+                num_steps=train_sequence_length + 1).prefetch(3)
+        iterator = iter(dataset)
+
+        def train_step():
+            experience, _ = next(iterator)
+            return tf_agent.train(experience)
+
+        if use_tf_functions:
+            train_step = common.function(train_step)
+
+        for _ in range(num_iterations):
+            start_time = time.time()
+            time_step, policy_state = collect_driver.run(
+                    time_step=time_step,
+                    policy_state=policy_state,
+                    )
+            for _ in range(train_steps_per_iteration):
+                train_loss = train_step()
+            time_acc += time.time() - start_time
+
+            if global_step.numpy() % log_interval == 0:
+                logging.info('step = %d, loss = %f', global_step.numpy(),
+                             train_loss.loss)
+                steps_per_sec = (global_step.numpy() - timed_at_step) / time_acc
+                logging.info('%.3f steps/sec', steps_per_sec)
+                tf.compat.v2.summary.scalar(
+                        name='global_steps_per_sec', data=steps_per_sec, step=global_step)
+                timed_at_step = global_step.numpy()
+                time_acc = 0
+
+            for train_metric in train_metrics:
+                train_metric.tf_summaries(
+                        train_step=global_step, step_metrics=train_metrics[:2])
+
+            if global_step.numpy() % train_checkpoint_interval == 0:
+                train_checkpointer.save(global_step=global_step.numpy())
+
+            if global_step.numpy() % policy_checkpoint_interval == 0:
+                policy_checkpointer.save(global_step=global_step.numpy())
+
+            if global_step.numpy() % rb_checkpoint_interval == 0:
+                rb_checkpointer.save(global_step=global_step.numpy())
+
+            if global_step.numpy() % eval_interval == 0:
+                results = metric_utils.eager_compute(
+                        eval_metrics,
+                        eval_tf_env,
+                        eval_policy,
+                        num_episodes=num_eval_episodes,
+                        train_step=global_step,
+                        summary_writer=eval_summary_writer,
+                        summary_prefix='Metrics',
+                        )
+                if eval_metrics_callback is not None:
+                    eval_metrics_callback(results, global_step.numpy())
+                metric_utils.log_metrics(eval_metrics)
+        return train_loss
 
 
 def train():
-    gin.parse_config_files_and_bindings(["/web_nav/gwob/configs/envdesign.gin"], None)
-    time.sleep(10)
-    os.system('xeyes')
-    # Create an empty environment.
-    env = web_environment.GMiniWoBWebEnvironment(
-            base_url="file:///web_nav/gwob/",
-            global_vocabulary=vocabulary_node.LockedVocabulary())
-    time.sleep(10)
+    gin.parse_config_file('./train.gin')
+    train_eval()
 
-    # Create a q network.
-    q_net = q_networks.DQNWebLSTM(vocab_size=env.local_vocab.max_vocabulary_size, return_state_value=True)
-    time.sleep(10)
-    # Sample a new design of the form {'number_of_pages': Integer, 'action': List[Integer], 'action_page': List[
-    # Integer]}.
-    # `action` denotes primitive indices and `action_page` denotes their corresponding page indices.
-    # Each item in the `action_page` should be less than `number_of_pages`.
-    # For this tutorial, we will randomly sample a design.
-    number_of_pages = np.random.randint(4) + 1
-    design = {'number_of_pages': number_of_pages,
-              'action': np.random.choice(np.arange(len(web_primitives.CONCEPTS)), 5),
-              'action_page': np.random.choice(np.arange(number_of_pages), 5)}
-    time.sleep(10)
-    # Design the actual environment.
-    env.design_environment(
-            design, auto_num_pages=True)
-    time.sleep(10)
-    # Reset the environment.
-    state = env.reset()
 
-    # Add batch dimension.
-    state = {key: np.expand_dims(tensor, axis=0) for key, tensor in state.items()}
-
-    # Get flattened logits and values.
-    logits, values = q_net(state)
-
-    # Get greedy action.
-    action = np.argmax(logits)
-
-    # Run the action.
-    new_state, reward, done, info = env.step(action)
+if __name__ == '__main__':
+    train()
