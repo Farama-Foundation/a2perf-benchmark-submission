706f64ce45605fc1857630d1f588d2a80ea80142
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2023-05-06 17:12:00 -0400

    updated inference code to read path from environment variable

--- web_nav/inference.py
+++ web_nav/inference.py
@@ -1,63 +1,68 @@
 import os
-import sys
 
-# Insert the directory at the beginning of sys.path
-sys.path.insert(0, os.getcwd())
-
-from tf_agents.utils import common
 import gym
 import numpy as np
 import tensorflow as tf
 from tf_agents.agents.dqn import dqn_agent
-
 from tf_agents.environments import suite_gym, tf_py_environment
 from tf_agents.trajectories import time_step as ts
+from tf_agents.utils import common
 
 from train import DQNLSTM
 
-ROOT_DIR = '/tmp/pycharm_project_494/rl-perf/rl_perf/submission/logs/difficulty_1_seed_47'
+
+# ROOT_DIR = '/tmp/pycharm_project_494/rl-perf/rl_perf/submission/logs/difficulty_1_seed_47'
 
 
 def load_model():
+    # get root dir from env var
+
+    root_dir = os.environ['ROOT_DIR']
+    # seed = os.environ['SEED']
+    # env_batch_size = os.environ['ENV_BATCH_SIZE']
+
+    root_dir = root_dir
+    print(root_dir)
+
     env_name = 'WebNavigation-v0'
     learning_rate = 1e-4
     max_vocab_size = 500
     seed = 32
-    train_dir = os.path.join(ROOT_DIR, 'train')
+    train_dir = os.path.join(root_dir, 'train')
     tf_env = tf_py_environment.TFPyEnvironment(suite_gym.load(environment_name=env_name,
                                                               spec_dtype_map={gym.spaces.Discrete: np.int32},
                                                               gym_kwargs={'difficulty': 1, 'seed': seed}))
 
     global_step = tf.compat.v1.train.get_or_create_global_step()
     q_net = DQNLSTM(
-            observation_spec=tf_env.observation_spec(),
-            action_spec=tf_env.action_spec(),
-            state_spec=(),
-            vocab_size=max_vocab_size if max_vocab_size is not None else tf_env.pyenv.envs[
-                0].env.local_vocab.max_vocabulary_size,
-            profile_value_dropout=0.0,
-            q_min=None,
-            q_max=None,
-            embedding_dim=100,
-            name='q_network',
-            latent_dim=50,
-            return_state_value=True)
+        observation_spec=tf_env.observation_spec(),
+        action_spec=tf_env.action_spec(),
+        state_spec=(),
+        vocab_size=max_vocab_size if max_vocab_size is not None else tf_env.pyenv.envs[
+            0].env.local_vocab.max_vocabulary_size,
+        profile_value_dropout=0.0,
+        q_min=None,
+        q_max=None,
+        embedding_dim=100,
+        name='q_network',
+        latent_dim=50,
+        return_state_value=True)
 
     tf_agent = dqn_agent.DqnAgent(
-            tf_env.time_step_spec(),
-            tf_env.action_spec(),
-            q_network=q_net,
-            optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate),
-            td_errors_loss_fn=common.element_wise_huber_loss,
-            name='dqn_agent'
-            )
+        tf_env.time_step_spec(),
+        tf_env.action_spec(),
+        q_network=q_net,
+        optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate),
+        td_errors_loss_fn=common.element_wise_huber_loss,
+        name='dqn_agent'
+    )
     eval_policy = tf_agent.policy
 
     policy_checkpointer = common.Checkpointer(
-            ckpt_dir=os.path.join(train_dir, 'policy'),
-            max_to_keep=3,
-            policy=eval_policy,
-            global_step=global_step)
+        ckpt_dir=os.path.join(train_dir, 'policy'),
+        max_to_keep=3,
+        policy=eval_policy,
+        global_step=global_step)
     policy_checkpointer.initialize_or_restore()
 
     return eval_policy
