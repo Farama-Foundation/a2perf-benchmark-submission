56e54bbbda8a7735b1b7a1ce88de418f4f5b5bf1
jasonjabbour <jjj4se@virginia.edu>
2023-07-14 15:19:03 -0400

    update parameters from sh file

--- quadruped_locomotion/debug/train.py
+++ quadruped_locomotion/debug/train.py
@@ -9,7 +9,7 @@ def train_eval(
     root_dir, 
     seed=0, 
     env_name='', 
-    parallel = True,
+    parallel_mode = True,
     parallel_cores = 8,
     timesteps_per_actorbatch = 4096, 
     optim_batchsize = 256,
@@ -21,7 +21,7 @@ def train_eval(
     setup_path = None,
     ):
     
-    if parallel:
+    if parallel_mode:
              
         try:
             mpi_command = f"mpiexec -n {parallel_cores} python {setup_path} --mode {mode} --int_save_freq {int_save_freq} --output_dir {output_dir} --seed {seed} --total_timesteps {total_timesteps} --int_save_freq {int_save_freq} {'--visualize' if visualize else ''}"
@@ -37,13 +37,28 @@ def train_eval(
     
 def train():
     gin.parse_config_file('./train.gin')
-    seed = int(os.environ['SEED'])
     root_dir = os.environ['ROOT_DIR']
+    seed = int(os.environ['SEED'])
+    total_timesteps= int(os.environ['TOTAL_ENV_STEPS'])
+    parallel_mode = os.environ['PARALLEL_MODE']
+    parallel_cores = int(os.environ['PARALLEL_CORES'])
+    mode = os.environ['MODE']  
+    visualize = os.environ['VISUALIZE']  
+    int_save_freq =  int(os.environ['INT_SAVE_FREQ'])
+    setup_path = os.environ['SETUP_PATH']
+    
     output_dir = os.path.join(root_dir, 'policies')          
         
     train_eval(root_dir=root_dir,
                seed=seed,
-               output_dir=output_dir
+               output_dir=output_dir, 
+               total_timesteps=total_timesteps,
+               parallel_mode=parallel_mode, 
+               parallel_cores=parallel_cores,
+               mode=mode, 
+               visualize=visualize, 
+               int_save_freq=int_save_freq, 
+               setup_path=setup_path
                )
     
 if __name__ == '__main__':
