955820b58a97d3d233e5f8431386a0547a44ce59
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2023-11-01 01:03:59 +0000

    added sonnet locally to fix dependencies

--- web_nav/inference.py
+++ web_nav/inference.py
@@ -27,6 +27,7 @@ def load_model():
     learning_rate = 1e-4
     max_vocab_size = 500
     seed = 32
+    designs = [{'number_of_pages': 1, 'action': [], 'action_page': [], }]
     train_dir = os.path.join(root_dir, 'train')
 
     # Load the global vocabulary
@@ -35,8 +36,7 @@ def load_model():
     global_vocab.restore(dict(global_vocab=global_vocab_dict))
     tf_env = tf_py_environment.TFPyEnvironment(suite_gym.load(environment_name=env_name,
                                                               spec_dtype_map={gym.spaces.Discrete: np.int32},
-                                                              gym_kwargs={'difficulty': 1,
-                                                                          'seed': seed,
+                                                              gym_kwargs={'designs': designs, 'seed': seed,
                                                                           'global_vocabulary': global_vocab, }))
 
     global_step = tf.compat.v1.train.get_or_create_global_step()
