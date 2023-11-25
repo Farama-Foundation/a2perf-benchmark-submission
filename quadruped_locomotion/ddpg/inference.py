57d43611bb54b99b147c1afe0c7ee9aca10e367c
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2023-11-25 20:37:17 +0000

    Supporting inference for both PPO and DDPG locomotion. Also adding initial files for ppo

--- quadruped_locomotion/ddpg/inference.py
+++ quadruped_locomotion/ddpg/inference.py
@@ -1,48 +1,34 @@
-import argparse
 import os.path
-import random
-import time
-import functools
 
-import numpy as np
-import tensorflow as tf
-from mpi4py import MPI
-from stable_baselines.common.noise import OrnsteinUhlenbeckActionNoise, NormalActionNoise
-from stable_baselines.ddpg.policies import MlpPolicy
+from absl import app
 
-from rl_perf.domains import quadruped_locomotion
 from ddpg_imitation import DDPGImitation
 
 
 def load_model(env):
-    root_dir = os.environ['ROOT_DIR']
-    seed = int(os.environ['SEED'])
-    print("root_dir:", root_dir)
-    print("seed:", seed)
-
-    policies_dir = os.path.join(root_dir, 'policies')
-    max_policy = functools.reduce(max, [int(x.split('_')[2]) for x in os.listdir(policies_dir)])
-    policy_path = os.path.join(policies_dir, f'rl_policy_{max_policy}_steps.zip')
-    print("policy_path:", policy_path)
-
-    model = DDPGImitation.load(policy_path, env=env)
-
-    return model
+  root_dir = os.environ['ROOT_DIR']
+  seed = int(os.environ['SEED'])
+  print("root_dir:", root_dir)
+  print("seed:", seed)
+  policy_path = os.path.join(root_dir, f'final_ddpg_policy.zip')
+  print("policy_path:", policy_path)
+  model = DDPGImitation.load(policy_path)
+  return model
 
 
 def infer_once(model, observation):
-    # the model is from stable baselines so use it to run inference on a single observation
-    action, _states = model.predict(observation)
-    return action
+  # the model is from stable baselines so use it to run inference on a single observation
+  action, _states = model.predict(observation)
+  return action
 
 
 def preprocess_observation(observation):
-    return observation
+  return observation
 
 
 def main(_):
-    pass
+  pass
 
 
 if __name__ == '__main__':
-    app.run(main)
+  app.run(main)
--- quadruped_locomotion/ppo/inference.py
+++ quadruped_locomotion/ppo/inference.py
@@ -4,37 +4,34 @@ import os
 import tensorflow as tf
 from absl import app
 from rl_perf.domains import quadruped_locomotion
-from rl_perf.domains.quadruped_locomotion.motion_imitation.learning import ppo_imitation, imitation_policies
+from rl_perf.domains.quadruped_locomotion.motion_imitation.learning import \
+  ppo_imitation, imitation_policies
 
 
 def load_model(env):
-    root_dir = os.environ['ROOT_DIR']
-    seed = int(os.environ['SEED'])
-    print("root_dir:", root_dir)
-    print("seed:", seed)
-
-    policies_dir = os.path.join(root_dir, 'policies')
-    max_policy = functools.reduce(max, [int(x.split('_')[2]) for x in os.listdir(policies_dir)])
-    policy_path = os.path.join(policies_dir, f'rl_policy_{max_policy}_steps.zip')
-    print("policy_path:", policy_path)
-
-    model = ppo_imitation.PPOImitation.load(policy_path, env=env)
-    return model
+  root_dir = os.environ['ROOT_DIR']
+  seed = int(os.environ['SEED'])
+  print("root_dir:", root_dir)
+  print("seed:", seed)
+  policy_path = os.path.join(root_dir, f'final_ppo_policy.zip')
+  print("policy_path:", policy_path)
+  model = ppo_imitation.PPOImitation.load(policy_path, env=env)
+  return model
 
 
 def infer_once(model, observation):
-    # the model is from stable baselines so use it to run inference on a single observation
-    action, _states = model.predict(observation)
-    return action
+  # the model is from stable baselines so use it to run inference on a single observation
+  action, _states = model.predict(observation)
+  return action
 
 
 def preprocess_observation(observation):
-    return observation
+  return observation
 
 
 def main(_):
-    pass
+  pass
 
 
 if __name__ == '__main__':
-    app.run(main)
+  app.run(main)
