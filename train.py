404309a92a306b8d09ce3ff24ab3f2035f20db3b
Ikechukwu Uchendu <iuchendu@g.harvard.edu>
2024-01-27 18:51:04 +0000

    removed num replicas flags

--- train.py
+++ train.py
@@ -55,7 +55,6 @@ def train():
 
   # Networking params
   num_collect_machines = int(os.environ.get('NUM_COLLECT_MACHINES', 1))
-  num_replicas = int(os.environ.get('NUM_REPLICAS', -1))
   replay_buffer_server_address = os.environ.get(
       'REPLAY_BUFFER_SERVER_ADDRESS', None
   )
